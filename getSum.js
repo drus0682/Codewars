2d88fb204fa3b0529653b63ec5a3afbf44cbf89b
ANDREW ZALIVAKO <drus0682@gmail.com>
2021-12-29 14:36:41 +0300

    Add files via upload

---
+++ getSum.js
@@ -0,0 +1,20 @@
+function getSum(a, b) {
+    let sum = 0;
+    if (a == b) {
+        return a;
+    }
+    if (a < b) {
+        sum += b;
+        while (a < b) {
+            sum += a;
+            a++;
+        }
+    }
+    if (a > b) {
+        sum += a;
+        while (a > b) {
+            sum += b;
+            b++;
+        }
+    }
+    return sum;
